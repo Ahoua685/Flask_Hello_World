adfedf28a9b4e39725719c1f2232e4dcb5a9fed4
Ahoua685 <ahouacool@gmail.com>
2025-01-04 17:19:10 +0100

    exo4

--- __init__.py
+++ __init__.py
@@ -24,7 +24,7 @@ def carre(val_user):
 @app.route('/somme/<int:valeur1>/<int:valeur2>')
 def somme(valeur1,valeur2):
     result= valeur1 + valeur2
-    return "<h2>La somme de votre valeur est:  </h2>{result}"
+    return "<h2>La somme de votre valeur est:  </h2> {result}"
 
 if __name__ == "__main__":
   app.run(debug=True)
